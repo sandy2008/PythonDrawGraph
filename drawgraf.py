210bdf34f68b60a0001e2c939b9d43125e9460d3
Sandy Chen <sandy2142@qq.com>
2017-12-04 17:06:45 +0900

    Update drawgraf.py

--- drawgraf.py
+++ drawgraf.py
@@ -1,12 +1,11 @@
 import numpy as np
 import cv2
 
-drawing = False #鼠标按下为真
-mode = True #如果为真，画矩形，按m切换为曲线
+
 ix,iy=-1,-1
 
 def draw_circle(event,x,y,flags,param):
-    global ix,iy,drawing,mode
+    global ix,iy
 
 
 
@@ -20,9 +19,5 @@ cv2.setMouseCallback('image',draw_circle)
 
 while(1):
     cv2.imshow('image',img)
-    k = cv2.waitKey(1) & 0xFF
-    if k == ord('m') :
-        mode = not mode
-    elif k == 27:
-        break
+
 cv2.destroyAllWindows()
